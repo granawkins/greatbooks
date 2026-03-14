ee00513aa6b7be41c387e0cc560818a07dd2a969
Clio <clio@greatbooks.fm>
2026-03-14 23:48:03 +0000

    Add annotations frontend: highlight, comment, range selection

--- src/components/reader/index.ts
+++ src/components/reader/index.ts
@@ -2,4 +2,4 @@ export { ChapterBlocks, ChapterDivider } from "./ChapterBlocks";
 export { BookHeader } from "./BookHeader";
 export { ChapterNav } from "./ChapterNav";
 export { groupIntoBlocks, buildWordSpans, paraTimeRange } from "./blockGrouping";
-export type { Block, ParagraphBlock, HeadingBlock, Segment, ChapterData, NavChapter, BookMeta, WordSpan } from "./types";
+export type { Block, ParagraphBlock, HeadingBlock, Segment, ChapterData, NavChapter, BookMeta, WordSpan, Annotation } from "./types";
